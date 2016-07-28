e1b11b45dcf087bc29f5f474f9ef14d1288c10cc
AlexBerg82 <BerdnykAleks@yahoo.com>
2016-07-29 00:12:27 +0300

    home_work_js2v2

--- HW_JS_3_4/js/main.js
+++ HW_JS_3_4/js/main.js
@@ -1,58 +1,65 @@
-﻿//объект
-var obj = {
-	//метод построения DOM
-	bildDom: function(){
-		//создание обертки
-		var wrapper = document.createElement('div');
-		wrapper.classList.add('box');
-		document.body.appendChild(wrapper);
+﻿//создание обертки
+var wrapper = document.createElement('div');
+wrapper.classList.add('box');
+document.body.appendChild(wrapper);
+var box = wrapper;
 
-		//создание заголовка
+//объект
+var obj = {
+	//функция вставки
+	appendElement: function(data) {
+		box.appendChild(data);
+	},
+	//создание заголовка
+	bildTitl: function(textTitl){
 		var titl = document.createElement('p');
 		titl.classList.add('titl');
-		titl.innerHTML = 'Тест по программированию';
-		//вставка заголовка в обертку
-		var box = document.querySelector('.box');	
-		box.appendChild(titl);
-
-		//создание пунктов
-		for(i=0; i<3; i++){
+		titl.innerHTML = textTitl;
+		this.appendElement(titl);
+	},
+	//создание кнопки
+	bildButn: function(textButn){
+		var butn = document.createElement('button');
+		butn.classList.add('butn');
+		butn.innerHTML = textButn;
+		this.appendElement(butn);
+	},
+	//создание пунктов
+	bildCheck: function(bc, ch, q, vq){
+		for(i=0; i<bc; i++){
 			var elems = document.createElement('p');
-			box.appendChild(elems);
-			elems.innerHTML = (i+1)+'. Вопрос №'+(i+1);
+			this.appendElement(elems);
+			elems.innerHTML = (i+1)+q+(i+1);
 
 		//создание обертки для каждой группы чекбоксов
 		var chkwrap = document.createElement('div');
 		chkwrap.classList.add('boxcheck','clearfix');
-		box.appendChild(chkwrap);
+		this.appendElement(chkwrap);
 
-		//создание группы чекбоксов
-				for(j=0; j<3; j++){
-					//создание чекбокса
-					var chkbox = document.createElement('input');
-					chkbox.type = "checkbox";
-					//создание id для чекбоксов
-					var aaa = i.toString();
-					var bbb = j.toString();
-					var chk = 'chk'+aaa+bbb;
-					chkbox.setAttribute('id',chk);
-					chkwrap.appendChild(chkbox);
-					
-					//создание подписи к чекбоксу
-					var lab = document.createElement('label');
-					chkwrap.appendChild(lab);
-					lab.setAttribute('for',chk);
-					lab.innerHTML = 'Вариант ответа № '+(j+1);
-				}
-		}
-
-		//создание кнопки и вставка в обертку
-		var butt = document.createElement('button');
-		butt.classList.add('butt');
-		butt.innerHTML = 'Проверить мои результаты';
-		box.appendChild(butt);
+			//создание группы чекбоксов
+			for(j=0; j<ch; j++){
+				//создание чекбокса
+				var chkbox = document.createElement('input');
+				chkbox.type = "checkbox";
+				//создание id для чекбоксов
+				var aaa = i.toString();
+				var bbb = j.toString();
+				var chk = 'chk'+aaa+bbb;
+				chkbox.setAttribute('id',chk);
+				this.appendElement(chkbox);
+						
+				//создание подписи к чекбоксу
+				var lab = document.createElement('label');
+				this.appendElement(lab);
+				lab.setAttribute('for',chk);
+				lab.innerHTML = vq+(j+1);
+			}
 	}
+}
+
 };
 
-//вызов метода объекта
-obj.bildDom();
+//вызов методов объекта
+obj.bildTitl('Тест по программированию');				//заголовок
+obj.bildCheck(3,3,'. Вопрос №','Вариант ответа № ');	//вопрос и варианты ответов
+obj.bildButn('Проверить мои результаты');				//кнопка
